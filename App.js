407092069ef32b09b81484dae455ab43680b215c
Karltheram <89616201+Karltheram@users.noreply.github.com>
2022-11-15 20:41:21 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import Home from "./components/Home";
+import Landing from "./components/Landing";
+import NavBar from "./components/NavBar";
+import WhatIsMe from "./components/WhatIsMe";
+
+function App() {
+  return (
+    <div>
+    <NavBar />
+    <Landing/>
+    <Home />
+    <WhatIsMe/>
+    </div>
+  );
+}
+
+export default App;
