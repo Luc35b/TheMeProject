a350dde3278e6052b53c07f1ba7766f85347e29b
Luc35b <baraya.lucas@ufl.edu>
2022-11-28 17:47:48 -0500

    Everything saves to database

---
+++ src/components/Stats.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ClientNav from './ClientNav';
+
+const Stats = () => {
+  return (
+    
+    <div name="Stats" className='nav-link w-full h-screen bg-gradient-to-b from-sky-500 via-sky-500 to-sky-300 text-white '>
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Stats
